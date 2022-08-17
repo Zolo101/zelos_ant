befdc3c349b5895aee330d96722253ffff94bfa9
Zolo101 <umadeguy@gmail.com>
2022-08-17 02:23:56 +0100

    Bump to 1.1.0

--- src/component/Credits.svelte
+++ src/component/Credits.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
-
     import Link from "./Link.svelte";
 </script>
 
 <div class="credits">
     <img src="zelos_ant_3x.png" alt="Zelo's Ant Logo"/>
-    <p class="credit">zelo's ant 1.0.0</p>
+<!--  TODO: Changelog  -->
+    <p class="credit">zelo's ant 1.1.0</p>
     <p class="credit">Made by Zelo101</p>
     <p>Libraries used:</p>
     <ul>
         <li><Link href="https://ramdajs.com/">Ramda</Link></li>
+        <li><Link href="https://dexie.org/">Dexie</Link></li>
         <li><Link href="https://developers.google.com/blockly">Blockly</Link></li>
         <li><Link href="https://www.assemblyscript.org/">AssemblyScript</Link></li>
         <li><Link href="https://kit.svelte.dev/">SvelteKit</Link></li>
