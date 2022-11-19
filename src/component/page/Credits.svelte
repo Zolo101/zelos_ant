3bcaaadf4b972d4eeadbef3319c3f75857a9c2b2
Zolo101 <umadeguy@gmail.com>
2022-11-19 02:16:39 +0000

    Bump to 1.2.0

--- src/component/page/Credits.svelte
+++ src/component/page/Credits.svelte
@@ -5,14 +5,15 @@
 <div class="credits">
     <img src="zelos_ant_3x.png" alt="Zelo's Ant Logo"/>
 <!--  TODO: Changelog  -->
-    <p class="credit">zelo's ant 1.1.0</p>
+    <p class="credit">zelo's ant 1.2.0</p>
     <p class="credit">Made by Zelo101</p>
     <p>Libraries used:</p>
     <ul>
+        <li><Link href="https://twgljs.org/">TWGL</Link></li>
         <li><Link href="https://ramdajs.com/">Ramda</Link></li>
         <li><Link href="https://dexie.org/">Dexie</Link></li>
         <li><Link href="https://developers.google.com/blockly">Blockly</Link></li>
-        <li><Link href="https://www.assemblyscript.org/">AssemblyScript</Link></li>
+<!--        <li><Link href="https://www.assemblyscript.org/">AssemblyScript</Link></li>-->
         <li><Link href="https://kit.svelte.dev/">SvelteKit</Link></li>
     </ul>
 </div>
